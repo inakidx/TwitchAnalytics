58d83ee7e7684a7d03ae96a327d83c9be4b6d780
inakidx <inakidx@gmail.com>
2024-10-15 16:13:38 +0200

    UPDATE clean repositories

---
+++ TwitchAnalytics.Data.Core/ApiClient.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
+using TwitchAnalytics.Data.Core.AccessToken;
+
+namespace TwitchAnalytics.Data.Core;
+
+public class ApiClient(AccessTokenService accessTokenService, IConfiguration configuration)
+{
+    private readonly AccessTokenService _accessTokenService = accessTokenService;
+    private readonly IConfiguration _configuration = configuration;
+
+    public async Task<HttpClient> GetAuthenticatedClient()
+    {
+        var token = await _accessTokenService.GetAccessToken();
+
+        HttpClient client = new();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Access_Token);
+        client.DefaultRequestHeaders.Add("Client-ID", _configuration.GetSection("AuthSettings:ClientId").Value);
+        return client;
+    }
+}
--- TwitchAnalytics.Data.Core/TwitchAPIRepository.cs
+++ TwitchAnalytics.Data.Core/TwitchAPIRepository.cs
@@ -2,58 +2,55 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Data;
-using System.Net.Http.Headers;
-using TwitchAnalytics.Data.Core.AccessToken;
 
 namespace TwitchAnalytics.Data.Core;
 
-public abstract class TwitchAPIRepository(IConfiguration configuration, AccessTokenService accessTokenService)
+public abstract class TwitchAPIRepository(IConfiguration configuration, ApiClient apiClient)
 {
     private readonly IConfiguration _configuration = configuration;
-    private readonly AccessTokenService _accessTokenService = accessTokenService;
+    private readonly ApiClient _apiClient = apiClient;
 
     public async Task<T> GetResource<T>(string resource, KeyValuePair<string, string>[] parameters)
     {
-        string typeName = typeof(T).Name;
-        var token = await _accessTokenService.GetAccessToken();
-
-        HttpClient client = new();
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Access_Token);
-        client.DefaultRequestHeaders.Add("Client-ID", _configuration.GetSection("AuthSettings:ClientId").Value);
-
-        string uri = _configuration.GetConnectionString("TwitchAPI") + "/" + resource;
-        string query = string.Empty;
-        if (parameters.Length != 0)
-        {
-            query = "?" + string.Join("&", parameters.Select(p => $"{p.Key}={p.Value}"));
-        }
-        var response = await client.GetAsync($"{uri}{query}");
+        HttpClient client = await _apiClient.GetAuthenticatedClient();
+        var response = await client.GetAsync(GetUrl(resource, parameters));
         response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync() ?? throw new KeyNotFoundException($"{typeName} not found");
-        JObject jsonObject = JObject.Parse(content);
-        var dataNode = jsonObject["data"] ?? throw new Exception($"{typeName} parse error");
 
-        return JsonConvert.DeserializeObject<T>(dataNode.ToString()) ?? throw new Exception($"{typeName} parse error");
+        return await GetParsedResource<T>(response);
     }
+
     public async Task<int> GetTotalResources(string resource, KeyValuePair<string, string>[] parameters)
     {
-        var token = await _accessTokenService.GetAccessToken();
+        HttpClient client = await _apiClient.GetAuthenticatedClient();
 
-        HttpClient client = new();
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Access_Token);
-        client.DefaultRequestHeaders.Add("Client-ID", _configuration.GetSection("AuthSettings:ClientId").Value);
-
-        string uri = _configuration.GetConnectionString("TwitchAPI") + "/" + resource;
-        string query = string.Empty;
-        if (parameters.Length != 0)
-        {
-            query = "?" + string.Join("&", parameters.Select(p => $"{p.Key}={p.Value}"));
-        }
-        var response = await client.GetAsync($"{uri}{query}");
+        string url = GetUrl(resource, parameters);
+        var response = await client.GetAsync(url);
         response.EnsureSuccessStatusCode();
         var content = await response.Content.ReadAsStringAsync() ?? throw new KeyNotFoundException($"total not found");
         JObject jsonObject = JObject.Parse(content);
         var dataNode = jsonObject["total"] ?? throw new Exception($"total parse error");
         return int.Parse(dataNode.ToString());
+    }
+
+    private string GetUrl(string resource, KeyValuePair<string, string>[] parameters)
+    {
+        string uri = _configuration.GetConnectionString("TwitchAPI") + "/" + resource;
+
+        string query = string.Empty;
+        if (parameters.Length != 0)
+        {
+            query = "?" + string.Join("&", parameters.Select(p => $"{p.Key}={p.Value}"));
+        }
+        return uri + query;
+    }
+
+    private static async Task<T> GetParsedResource<T>(HttpResponseMessage response)
+    {
+        string typeName = typeof(T).Name;
+        var content = await response.Content.ReadAsStringAsync() ?? throw new KeyNotFoundException($"{typeName} not found");
+        JObject jsonObject = JObject.Parse(content);
+        var dataNode = jsonObject["data"] ?? throw new Exception($"{typeName} parse error");
+
+        return JsonConvert.DeserializeObject<T>(dataNode.ToString()) ?? throw new Exception($"{typeName} parse error");
     }
 }
